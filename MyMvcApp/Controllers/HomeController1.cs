2bd011412163b3ae48db7386a2483f5ef21a8336
ptanh05 <148223045+ptanh05@users.noreply.github.com>
2025-08-26 15:15:17 +0700

    nộp bài lần 1 Tạo Project ASP.NET Core MVC

---
+++ MyMvcApp/Controllers/HomeController1.cs
@@ -0,0 +1,18 @@
+﻿// Renamed to avoid duplicate 'HomeController' definition in the same namespace
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyMvcApp.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyMvcApp/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyMvcApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
