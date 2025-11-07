37bc80726fc0a9ad7b5798f9f34a5c53d3e02fc1
Phung The Anh <anh018031@gmail.com>
2025-11-07 09:06:37 +0700

    Add jQuery practice project (Pta_JQuery)

---
+++ Pta_JQuery/Pta_JQuery/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pta_JQuery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
